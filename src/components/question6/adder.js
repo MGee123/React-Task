0714c3b117347f57bc642ca77dda5ffe3becbaf2
MG <mattgordon@hotmail.co.uk>
2017-02-23 15:46:39 +0000

    question6

---
+++ src/components/question6/adder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Adder extends React.Component {
+render () {
+return (
+
+<p>{this.props.number1} + {this.props.number2} = {this.props.number1 + this.props.number2}</p>
+);
+}
+}
+
+
---
+++ src/components/question6/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Adder from './adder'
+
+export default () => 
+(<p><Adder number1={1} number2={2} /></p>)
+
