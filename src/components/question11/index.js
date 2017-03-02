a7edb36a62fa7ff1679aac90a21b9c1f0f07a630
MG <mattgordon@hotmail.co.uk>
2017-03-02 10:39:48 +0000

    11

--- src/components/question11/index.js
+++ src/components/question11/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Propson from '../question5/propson'
 import Company from './zdev'
 
 export default () => <Company name={'ZDEV'} Employees={[{name: 'John', age: 20}, {name: 'Rich', age: 27}]} />
--- src/components/question11/zdev.js
+++ src/components/question11/zdev.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Propson from '../question5/propson'
+
 export default class Company extends React.Component {
  render () {
+
  return (  <div>
 	<p>Company: { this.props.name }</p>
 	
-    <p>Employees: { this.props.person }</p>
+    <p>Employees: <Propson person={{name: 'John', age: 20}}/> 
+	<Propson person={{name: 'Rich', age: 27}} /></p>
        
 </div>
 );
